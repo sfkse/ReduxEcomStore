a51c5997198fad05657ff2357944d7744b14f9a7
sfkse <sfkse5591@gmail.com>
2021-10-31 18:10:44 +0200

    Show cart quantity with cart icon

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,17 +1,15 @@
 import { IconContext } from 'react-icons'
 import { FaShoppingCart } from 'react-icons/fa';
-import { useDispatch, useSelector } from 'react-redux';
-import { addCart, addCartPrice } from '../app/actionCreator';
+import { useDispatch } from 'react-redux';
+import { addCart } from '../app/actionCreator';
 
 const Product = (product) => {
-    const cartItems = useSelector(state => state.cart)
     const dispatch = useDispatch()
 
 
     const handleAddCart = (addProduct) => {
 
         dispatch(addCart(addProduct))
-        // dispatch(addCartPrice(addProduct))
     }
     return (
         <div className="shop__card">

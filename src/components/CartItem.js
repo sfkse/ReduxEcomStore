041c837887219a73638c35ca2d61e517303c9e0b
sfkse <sfkse5591@gmail.com>
2021-10-31 10:53:36 +0200

    Fix cart calculation

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { IconContext } from 'react-icons'
 import { FaRegWindowClose } from 'react-icons/fa'
 import { useDispatch } from 'react-redux';
@@ -7,27 +7,33 @@ import { deleteCartItem, deleteCartPrice, updateCartPrice } from '../app/actionC
 
 const CartItem = ({ item }) => {
     const [qty, setQty] = useState(1)
-    const [price, setPrice] = useState(item.price)
     const cartItems = useSelector(state => state.cart);
+    const cartPrices = useSelector(state => state.cartPrice);
     const dispatch = useDispatch();
     const index = cartItems.findIndex(updated => updated === item);
+    const qtyOfItems = cartPrices[index] / item.price;
     let finalPrice;
+
+    useEffect(() => {
+        setQty(qtyOfItems)
+    }, [qtyOfItems])
+
 
     const handleChange = (e) => {
         setQty(e.target.value)
     }
     const handleAddQty = () => {
         setQty(qty + 1)
+        //using qty+1 because of delayed render
         finalPrice = (qty + 1) * item.price
-        setPrice(finalPrice)
         dispatch(updateCartPrice(finalPrice, index))
 
     }
     const handleDecreaseQty = () => {
         if (qty > 1) {
             setQty(qty - 1)
+            //using qty-1 because of delayed render
             finalPrice = (qty - 1) * item.price
-            setPrice(finalPrice)
             dispatch(updateCartPrice(finalPrice, index))
         }
     }
@@ -53,7 +59,7 @@ const CartItem = ({ item }) => {
                     readOnly />
                 <button className="cart__item--increase" onClick={handleAddQty}>+</button>
             </span>
-            <span className="cart__item--price">${price}</span>
+            <span className="cart__item--price">${qtyOfItems * item?.price}</span>
             <span className="cart__item--delete" onClick={() => handleDelete(item?.id)}>
                 <IconContext.Provider value={{ className: "cart__item--icon" }}>
                     <FaRegWindowClose />
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,15 @@
-import React from 'react'
 import { IconContext } from 'react-icons'
 import { FaShoppingCart } from 'react-icons/fa';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addCart, addCartPrice } from '../app/actionCreator';
 
 const Product = (product) => {
+    const cartItems = useSelector(state => state.cart)
     const dispatch = useDispatch()
 
+
     const handleAddCart = (addProduct) => {
+
         dispatch(addCart(addProduct))
         dispatch(addCartPrice(addProduct))
     }

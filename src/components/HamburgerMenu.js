df75bba5364ec1401dc85880a657688578005403
sfkse <sfkse5591@gmail.com>
2021-11-02 00:08:40 +0200

    Update css

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -8,6 +8,7 @@ const toggleScroll = document.querySelector('body')
 
 const HamburgerMenu = () => {
     const [showMenu, setShowMenu] = useState(false)
+
     const handleClick = () => {
         setShowMenu(!showMenu)
         toggleScroll.classList.toggle('scroll__hidden')
@@ -34,8 +35,8 @@ const HamburgerMenu = () => {
         } :
             {
                 to: { opacity: 0, left: 1000 },
-                // from: { opacity: 1, left: 0 },
-                delay: 300
+                from: { opacity: 1, left: 0 },
+                delay: 500
             })
     return (
 
@@ -48,14 +49,14 @@ const HamburgerMenu = () => {
 
             {
 
-                <animated.div style={props} className={`hamburger ${showMenu ? 'd-flex' : 'd-none'}`}>
+                <animated.div style={props} className={`hamburger  ${showMenu ? 'd-flex' : 'd-none'}`}>
                     <h1 className="hamburger__logo">Just Shop.</h1>
                     <ul className="hamburger__menu">
                         <li className="hamburger__item" >
-                            <Link to="/" className="hamburger__link" >Home</Link>
+                            <Link to="/" className="hamburger__link" onClick={handleClick}>Home</Link>
                         </li>
                         <li className="hamburger__item" >
-                            <a href="/contact" className="hamburger__link" >Contact</a>
+                            <a href="/contact" className="hamburger__link" onClick={handleClick}>Contact</a>
                         </li>
                     </ul>
                     <div className="hamburger__search">
@@ -63,11 +64,11 @@ const HamburgerMenu = () => {
                     </div>
                     <div className="hamburger__account">
                         <IconContext.Provider value={{ className: "hamburger__icon" }}>
-                            <FaUserAlt />
+                            <FaUserAlt onClick={handleClick} />
                         </IconContext.Provider>
                         <Link to="/cart" className="hamburger__cart">
                             <IconContext.Provider value={{ className: "hamburger__icon" }}>
-                                <FaShoppingCart />
+                                <FaShoppingCart onClick={handleClick} />
                             </IconContext.Provider>
                             {/* <span className="navbar__icon navbar__cart--qty">{cartItems.cartItems.length}</span> */}
                         </Link>
